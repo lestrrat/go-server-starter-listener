7ef1940e4446a9796369eab1781e3cd3b1984595
Daisuke Maki <lestrrat+github@gmail.com>
2014-04-25 14:53:52 +0900

    I'm a born against go-hard-tabber

--- listener.go
+++ listener.go
@@ -1,17 +1,17 @@
 package ss
 
 import (
-  "fmt"
-  "net"
-  "os"
-  "strconv"
-  "strings"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 )
 
 // ListenTarget desribes an address and an associated file descriptor
 type ListenTarget struct {
-  Name  string // host:port | port. Currently unix sockets are not supported
-  Fd    uintptr
+	Name string // host:port | port. Currently unix sockets are not supported
+	Fd   uintptr
 }
 
 /*
@@ -21,12 +21,12 @@ of ListenTarget structs that can be passed to NewListenerOn()
 
 */
 func Ports() ([]ListenTarget, error) {
-  ssport := os.Getenv("SERVER_STARTER_PORT")
-  if ssport == "" {
-    return nil, fmt.Errorf("error: No environment variable SERVER_STARTER_PORT available")
-  }
+	ssport := os.Getenv("SERVER_STARTER_PORT")
+	if ssport == "" {
+		return nil, fmt.Errorf("error: No environment variable SERVER_STARTER_PORT available")
+	}
 
-  return ParsePorts(ssport)
+	return ParsePorts(ssport)
 }
 
 /*
@@ -36,35 +36,35 @@ of ListenTarget structs that can be passed to NewListenerOn()
 
 */
 func ParsePorts(ssport string) ([]ListenTarget, error) {
-  ret := []ListenTarget{}
-  for _, pairstring := range strings.Split(ssport, ";") {
-    pair := strings.Split(pairstring, "=")
-    port, err := strconv.ParseUint(pair[1], 10, 0)
-    if err != nil {
-      return nil, fmt.Errorf("error: Failed to parse '%s'", pairstring)
-    }
-    ret = append(ret, ListenTarget { pair[0], uintptr(port) })
-  }
-  return ret, nil
+	ret := []ListenTarget{}
+	for _, pairstring := range strings.Split(ssport, ";") {
+		pair := strings.Split(pairstring, "=")
+		port, err := strconv.ParseUint(pair[1], 10, 0)
+		if err != nil {
+			return nil, fmt.Errorf("error: Failed to parse '%s'", pairstring)
+		}
+		ret = append(ret, ListenTarget{pair[0], uintptr(port)})
+	}
+	return ret, nil
 }
 
 /*
 
-NewListenerOrDefault creates a new listener from SERVER_STARTER_PORT, or 
+NewListenerOrDefault creates a new listener from SERVER_STARTER_PORT, or
 if that fails, binds to the "default" bind address
 
 */
 func NewListenerOrDefault(proto, defaultBindAddress string) (net.Listener, error) {
-  l, err := NewListener()
-  if err == nil {
-    return l, nil
-  }
+	l, err := NewListener()
+	if err == nil {
+		return l, nil
+	}
 
-  dl, err := net.Listen(proto, defaultBindAddress)
-  if err == nil {
-    return dl, nil
-  }
-  return nil, err
+	dl, err := net.Listen(proto, defaultBindAddress)
+	if err == nil {
+		return dl, nil
+	}
+	return nil, err
 }
 
 /*
@@ -74,29 +74,29 @@ NewListener creates a new listener from SERVER_STARTER_PORT environment variable
 Note that this binds to only ONE file descriptor.
 If multiple ports are specified in the environment variable, the first one is used
 
- */
+*/
 func NewListener() (net.Listener, error) {
-  portmap, err := Ports()
-  if err != nil {
-    return nil, err
-  }
-  return NewListenerOn(portmap[0])
+	portmap, err := Ports()
+	if err != nil {
+		return nil, err
+	}
+	return NewListenerOn(portmap[0])
 }
 
-/* 
+/*
 
-AllListeners creates new listeners from SERVER_STARTER_PORT environment 
+AllListeners creates new listeners from SERVER_STARTER_PORT environment
 variable.
 
 This binds to ALL file descriptors in SERVER_STARTER_PORT
 
 */
 func AllListeners() ([]net.Listener, error) {
-  portmap, err := Ports()
-  if err != nil {
-    return nil, err
-  }
-  return NewListenersOn(portmap)
+	portmap, err := Ports()
+	if err != nil {
+		return nil, err
+	}
+	return NewListenersOn(portmap)
 }
 
 /*
@@ -104,16 +104,16 @@ func AllListeners() ([]net.Listener, error) {
 NewListenersOn creates listeners for each ListenTarget given
 
 */
-func NewListenersOn (list []ListenTarget) ([]net.Listener, error) {
-  ret := []net.Listener {}
-  for _, t := range list {
-    l, err := NewListenerOn(t)
-    if err != nil {
-      return nil, err
-    }
-    ret = append(ret, l)
-  }
-  return ret, nil
+func NewListenersOn(list []ListenTarget) ([]net.Listener, error) {
+	ret := []net.Listener{}
+	for _, t := range list {
+		l, err := NewListenerOn(t)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, l)
+	}
+	return ret, nil
 }
 
 /*
@@ -121,7 +121,7 @@ func NewListenersOn (list []ListenTarget) ([]net.Listener, error) {
 NewListenerOn creates a listefer for given ListenTarget
 
 */
-func NewListenerOn (t ListenTarget) (net.Listener, error) {
-  f := os.NewFile(t.Fd, t.Name)
-  return net.FileListener(f)
+func NewListenerOn(t ListenTarget) (net.Listener, error) {
+	f := os.NewFile(t.Fd, t.Name)
+	return net.FileListener(f)
 }
--- listener_test.go
+++ listener_test.go
@@ -1,22 +1,22 @@
 package ss
 
-import(
-  "os"
-  "testing"
+import (
+	"os"
+	"testing"
 )
 
 func TestPort(t *testing.T) {
-  os.Setenv("SERVER_STARTER_PORT", "8080=4")
-  ports, err := Ports()
-  if err != nil {
-    t.Errorf("Failed to parse ports from env: %s", err)
-  }
+	os.Setenv("SERVER_STARTER_PORT", "8080=4")
+	ports, err := Ports()
+	if err != nil {
+		t.Errorf("Failed to parse ports from env: %s", err)
+	}
 
-  if len(ports) <= 0 {
-    t.Errorf("no ports found?!")
-  }
+	if len(ports) <= 0 {
+		t.Errorf("no ports found?!")
+	}
 
-  if ports[0].Fd != 4 {
-    t.Errorf("fd is not what we expected")
-  }
+	if ports[0].Fd != 4 {
+		t.Errorf("fd is not what we expected")
+	}
 }
